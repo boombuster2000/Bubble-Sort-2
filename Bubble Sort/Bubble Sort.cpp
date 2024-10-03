e7e1c2890334a1191ea80ad991b02e1ddd5ae14f
boombuster2000 <chanuka.williams@outlook.com>
2024-10-03 13:08:00 +0100

    Added function to print int array.

--- Bubble Sort/Bubble Sort.cpp
+++ Bubble Sort/Bubble Sort.cpp
@@ -21,15 +21,29 @@ void bubble_sort(int unsorted_data[], int unsorted_data_length)
     } while (swap);
 }
 
+
+void print_int_array(int array_variable[], int array_length)
+{
+    for (int i = 0; i < array_length; i++)
+    {
+        std::cout << array_variable[i];
+        if (i != array_length - 1) std::cout << ",";
+    }
+
+    std::cout << "\n";
+    
+}
 int main()
 {
     int unsorted_data [] = { 4,23,6,5,78,9,7,24,6565,3 };
     int unsorted_data_length = sizeof(unsorted_data) / sizeof(unsorted_data[0]);
     
-    for (int i = 0; i < unsorted_data_length; i++)std::cout << unsorted_data[i] << ", ";
-    std::cout << "\n";
+    std::cout << "Before:\t";
+    print_int_array(unsorted_data, unsorted_data_length);
 
     bubble_sort(unsorted_data, unsorted_data_length);
     
-    for (int i = 0; i < unsorted_data_length; i++) std::cout << unsorted_data[i] << ", ";
+    std::cout << "After:\t";
+    print_int_array(unsorted_data, unsorted_data_length);
 }
+;
